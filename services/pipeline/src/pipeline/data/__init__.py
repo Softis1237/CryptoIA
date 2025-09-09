2e533ba7ecbd3d06482f9f8b1517953988d38589
Softis1237 <strelkoff04@gmail.com>
2025-09-09 12:31:28 +0300

    feat: store onchain metrics locally

--- services/pipeline/src/pipeline/data/__init__.py
+++ services/pipeline/src/pipeline/data/__init__.py
@@ -1,10 +1,15 @@
-codex/create-ingest_news.py-for-news-ingestion
-from .ingest_news import IngestNewsInput, IngestNewsOutput, run
+"""Data ingestion modules."""
+
+from .ingest_news import IngestNewsInput, IngestNewsOutput
+from .ingest_news import run as run_news
+from .ingest_onchain import IngestOnchainInput, IngestOnchainOutput
+from .ingest_onchain import run as run_onchain
 
 __all__ = [
     "IngestNewsInput",
     "IngestNewsOutput",
-    "run",
+    "run_news",
+    "IngestOnchainInput",
+    "IngestOnchainOutput",
+    "run_onchain",
 ]
-"""Data ingestion modules."""
- main
